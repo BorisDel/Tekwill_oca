3ff1f479bfc1c5912b5450fa4186ca79fa8be10c
BorisDel <stud2me@gmail.com>
2018-04-26 19:41:26 +0300

    My forth activity"

---
+++ practice_04_1/src/ShoppingCart.java
@@ -0,0 +1,12 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        // Declare and initialize String variables.  Do not initialize message yet.
+            String custName = "Mary Smith", itemDesc = "Shirt"; String message;
+        // Assign the message variable 
+            message = custName + "vants to purchase a good " + itemDesc;
+        // Print and run the code
+    System.out.println (message);
+    }
+}
